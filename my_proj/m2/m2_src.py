89e67c45c1a66dc3a35219a6d9c859304d4204be
Raghavan Viswanathan <raghavan@riptideio.com>
2017-02-14 06:02:59 +0530

    master: Modified for blog

--- my_proj/m2/m2_src.py
+++ my_proj/m2/m2_src.py
@@ -4,8 +4,8 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 def m2f():
-    logger.critical("Appearing since log level for {} >= INFO".format(__name__))
-    logger.error("Appearing since log level for {} >= INFO".format(__name__))
-    logger.warning("Appearing since log level for {} >= INFO".format(__name__))
-    logger.info("Appearing since log level for {} >= INFO".format(__name__))
-    logger.debug("Not Appearing since log level for {} >= INFO".format(__name__))
+    logger.critical("Appearing since log level >= INFO")
+    logger.error("Appearing since log level >= INFO")
+    logger.warning("Appearing since log level >= INFO")
+    logger.info("Appearing since log level >= INFO")
+    logger.debug("Not Appearing since log level >= INFO")
